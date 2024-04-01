c44d54dda570df598377aa27e0ad9431ac38bdf3
Mitchell Dederer <mitchelldederer@gmail.com>
2024-04-01 18:24:45 +1100

    feat: added api

---
+++ api/IdeaManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Albury.Function
+{
+    public static class IdeaManager
+    {
+        [FunctionName("IdeaManager")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult(new { message = "Hello from IdeaManager"});
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>Hello, world!</div>
+
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("/api/IdeaManager")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Albury POC</h1>
+      {data && <p>{data}</p>}
+    </div>
+  )
 }
 
 export default App
